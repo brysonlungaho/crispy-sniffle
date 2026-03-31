9ef07e5f8fbd71717cb5708d6ef0f08e9823fbcd
Bryson Masivo <b.masivo@alustudent.com>
2026-03-31 19:52:29 +0200

    Add example configuration file for API keys
    Provide a template for configuration with API keys.

---
+++ config.example.js
@@ -0,0 +1,6 @@
+// Copy this file, rename it to config.js, and fill in your real API keys
+const CONFIG = {
+  WEATHER_KEY: 'YOUR_OPENWEATHER_KEY_HERE',
+  NEWS_KEY:  'YOUR_NEWSAPI_KEY_HERE'
+  REST_KEY: 'ÝOUR_REST_COUNTRIES_KEYS_HERE'
+};
